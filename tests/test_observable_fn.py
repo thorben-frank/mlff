1f27a7b6e1258dde86227d927c6c61d82ad9a3f7
Thorben Frank <thorbenjan.frank@gmail.com>
2024-01-09 09:41:38 +0100

    bugfix import path StackNetSparse in tests

--- tests/test_observable_fn.py
+++ tests/test_observable_fn.py
@@ -4,7 +4,7 @@ import jax
 import jax.numpy as jnp
 import jraph
 from mlff.geometric import get_rotation_matrix
-from mlff.nn import StackNetSparse
+from mlff.nn.stacknet import StackNetSparse
 from mlff.nn.embed import GeometryEmbedSparse, AtomTypeEmbedSparse
 from mlff.nn.layer import SO3kratesLayerSparse
 from mlff.nn.observable import EnergySparse
